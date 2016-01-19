a3adaf3b901af3506dc671a613443362a8405ecc
Cortney Lane <cdlane124@gmail.com>
2016-01-18 21:10:41 -0800

    add responsive images to descriptions

--- portProjects.js
+++ portProjects.js
@@ -1,15 +1,27 @@
 var portData = [
   {
-    title: 'Google',
-    publishedOn: '2016-01-08',
-    link: 'http://www.google.com',
-    description: "<p> I didn't actually make Google, but wouldn't it be cool if I did??</p></p>...Okay, okay, I'll remove this entry eventually...</p>"
+    title: 'Group Project - Code Fellows Restaurant Finder',
+    publishedOn: '2016-01-18',
+    link: 'http://cdl124.github.io/restaurant-finder-group-project',
+    description: "<p>This was our final project at the end of the 201 course. Our group decided on creating a website that finds all restaurants close to the Code Fellows Seattle campus. The big challenge with this project was using the Google Maps and Google Places API. The Maps API created an interactive map displayed in the middle of our homepage, and Places provided all the data on the nearby restaurants, which we then used to display the user's search results if they use the queries on the left side of the page.</p><p>The image below may or may not represent the state I was in when I originally pitched the 'restaurant finder' idea to my group...</p><img src='images/hungrywoman.jpg' />"
+  },
+  {
+  title: 'Cutest Kitten Contest Vote Tracker',
+  publishedOn: '2016-01-17',
+  link: 'http://cdl124.github.io/vote-tracker',
+  description: "<p>The \"Cutest Kitten Contest\" is a website that allows users to view two randomly-generated images of kittens at a time. The user then clicks on the kitten they find the cutest of the two, and three things happen:</p><ol><li>The selected image is highlighted in yellow,</li><li>One vote is logged for that image,which shows in the bar chart below the \"Get two more kitties!\" button,</li><li>and the website will also log one vote for that image in the user's voting history.</li><ol><img src='images/kitten.jpg' />"
+  },
+  {
+    title: 'Doughnut Shop Simulated Demand Webpage',
+    publishedOn: '2016-01-17',
+    link: 'http://cdl124.github.io/donut-shop',
+    description: "<p>For this project, our class was given a spreadsheet containing data on five different Seattle locations of the Top Pot Doughnuts franchise. The data included each location's minimum and maximum number of customers per hour, and the average number of donuts sold per customer. We then simulated daily doughnut demand at each location by creating a Javascript function that generates a random number between the minimum and maximum customers per hour at the respective locations, and calculating how many doughnuts are then sold at the end of each day.</p><p>This project also required more extensive use of CSS and design principles, as witnessed by the presence of a color theme, and at least one 'fixed' module, one 'absolute' module, and one 'relative' module.</p><img src='images/donuts.jpg' />"
   },
   {
     title: 'Guessing Game',
     publishedOn: '2016-01-09',
     link: 'http://cdl124.github.io',
-    description: "<p>This is my really rudimentary guessing game that I made in 201! This was also my first website that used HTML, CSS, and JavaScript to make an interactive online experience.</p>"
+    description: "<p>This is the guessing game that I created for the first project we had in the Code Fellows 201 class. This was also my first website that incorporated HTML, CSS, and JavaScript to make an interactive online experience. Before this project, each webpage I had created were completely static, using only HTML and CSS.</p><img src='images/questionmarks.png' />"
   }
 ]
 // something
